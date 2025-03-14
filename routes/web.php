ab553a729b52ab4ec58acd7e8681dd6ba068e13a
Lucas <thanhnv30072000@gmail.com>
2025-03-15 00:46:31 +0700

    day 3

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,35 @@
 <?php
 
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
-Route::get('/', function () {
-    return view('welcome');
+Route::group(['prefix' => ''], function(){
+    Route::get('/', [HomeController::class, 'index'])->name('home');
+    Route::get('/', [HomeController::class, 'index'])->name('home');
+    Route::get('/', [HomeController::class, 'index'])->name('home');
 });
+
+Route::group(['prefix' => 'admin'], function(){
+    Route::get('/', [AdminController::class, 'index'])->name('admin.index');
+
+    Route::group(['prefix' => 'category'], function(){
+        Route::get('/', [CategoryController::class, 'index'])->name('category.index');
+    }); 
+
+        
+});
+
+
+
+
+
+
+
+
+
